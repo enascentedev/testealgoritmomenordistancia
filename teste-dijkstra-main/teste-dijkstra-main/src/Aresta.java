b49457161ff811a9c41b90c3d7593613ae95500b
enascentedev <106539519+enascentedev@users.noreply.github.com>
2022-10-25 11:13:23 -0300

    substituição dos arquivos

---
+++ teste-dijkstra-main/teste-dijkstra-main/src/Aresta.java
@@ -0,0 +1,42 @@
+public class Aresta {
+
+  private double peso;
+  private Vertice origem;
+  private Vertice destino;
+
+  public Aresta(double peso, Vertice origem, Vertice destino) {
+    this.peso = peso;
+    this.origem = origem;
+    this.destino = destino;
+  }
+
+  public Aresta(Vertice v1, Vertice v2) {
+    this.peso = 1;
+    this.origem = v1;
+    this.destino = v2;
+  }
+
+  public void setPeso(double novoPeso) {
+    this.peso = novoPeso;
+  }
+
+  public double getPeso() {
+    return peso;
+  }
+
+  public void setDestino(Vertice destino) {
+    this.destino = destino;
+  }
+
+  public Vertice getDestino() {
+    return destino;
+  }
+
+  public void setOrigem(Vertice origem) {
+    this.origem = origem;
+  }
+
+  public Vertice getOrigem() {
+    return origem;
+  }
+}
